25dea97ad4fe36f2fadf557b9ce8fd757863a6ec
Nazareh <nazarehturmina@gmail.com>
2024-03-12 14:35:35 +0800

    Initial commit

---
+++ src/main/java/com/turminaz/myratingapp/MyRatingAppApplication.java
@@ -0,0 +1,13 @@
+package com.turminaz.myratingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRatingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRatingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turminaz/myratingapp/MyRatingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turminaz.myratingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRatingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
