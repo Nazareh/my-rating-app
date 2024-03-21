a980d1aba360ed10eec1e9747cfdf0eb7f87e8c5
Nazareh Velho Turmina <nturmina@maiatechnology.com.au>
2024-03-21 17:27:34 +0800

    Fix Tests

--- src/main/java/com/turminaz/myratingapp/config/FirebaseConfig.java
+++ src/main/java/com/turminaz/myratingapp/config/FirebaseConfig.java
@@ -1,11 +1,14 @@
 package com.turminaz.myratingapp.config;
 
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.NoCredentialsProvider;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
@@ -21,8 +24,8 @@ class FirebaseConfig {
     @Value("${spring.cloud.gcp.project-id}")
     private String projectId;
 
-    @Value("${spring.cloud.gcp.credentials.location}")
-    private Resource credentials;
+    @Value("${spring.cloud.gcp.credentials.location:#{null}}")
+    private Resource credentials = null;
 
     @Bean
     FirebaseApp firebaseApp() throws IOException {
@@ -44,6 +47,13 @@ class FirebaseConfig {
                     .build();
         }
         return FirebaseApp.initializeApp(options);
+    }
+    @Bean
+    @ConditionalOnProperty(
+            value = "spring.cloud.gcp.firestore.emulator.enabled",
+            havingValue = "true")
+    public CredentialsProvider googleCredentials() {
+        return NoCredentialsProvider.create();
     }
 
     @Bean
