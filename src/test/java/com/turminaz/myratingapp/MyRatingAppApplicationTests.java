0c202d457c8865bff6d002be38c83814774126cb
Nazareh Velho Turmina <nturmina@maiatechnology.com.au>
2024-12-04 14:11:01 +0800

    Disable ContextLoads test

--- src/test/java/com/turminaz/myratingapp/MyRatingAppApplicationTests.java
+++ src/test/java/com/turminaz/myratingapp/MyRatingAppApplicationTests.java
@@ -1,12 +1,13 @@
 package com.turminaz.myratingapp;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
-
+@Disabled
 class MyRatingAppApplicationTests {
 
 	@MockitoBean
