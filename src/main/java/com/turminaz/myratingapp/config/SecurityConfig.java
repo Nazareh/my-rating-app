45427527fcef73be135d22395d60e5bb3929de5c
Nazareh Velho Turmina <nturmina@maiatechnology.com.au>
2024-12-06 07:47:21 +0800

    Change players endpoint security

--- src/main/java/com/turminaz/myratingapp/config/SecurityConfig.java
+++ src/main/java/com/turminaz/myratingapp/config/SecurityConfig.java
@@ -24,7 +24,7 @@ class SecurityConfig {
                         .requestMatchers("/actuator/health").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs*/**").permitAll()
-                        .requestMatchers("/api/v1/player/**").permitAll()
+                        .requestMatchers("/api/v1/player*/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
                 .csrf(AbstractHttpConfigurer::disable);
